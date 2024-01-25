54a0f922e26352f3f5907d2b36fddc3e65106b82
github-action-benchmark <github@users.noreply.github.com>
2024-01-25 21:57:19 +0000

    add Benchmark (cargo) benchmark result for 83c9bc23f11cf690c6b502fde814eb623980b5ed

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1706219839540,
+  "repoUrl": "https://github.com/succinctlabs/vm",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jtguibas@gmail.com",
+            "name": "John Guibas",
+            "username": "jtguibas"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "83c9bc23f11cf690c6b502fde814eb623980b5ed",
+          "message": "feat: perf ci pt2 (#127)",
+          "timestamp": "2024-01-25T13:53:31-08:00",
+          "tree_id": "8e4546f8f1a08b94f3d2310c87a58b8b014da56d",
+          "url": "https://github.com/succinctlabs/vm/commit/83c9bc23f11cf690c6b502fde814eb623980b5ed"
+        },
+        "date": 1706219837673,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "prove/fibonacci: cycles=188618",
+            "value": 13218956974,
+            "range": "± 385476849",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
