30cb82c31eb94047402359bf990939b77f4a615e
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-02-29 12:45:56 -0800

    increase test to 5k keccak's

--- tests/keccak-permute/src/main.rs
+++ tests/keccak-permute/src/main.rs
@@ -4,7 +4,7 @@ sp1_zkvm::entrypoint!(main);
 use sp1_zkvm::syscalls::syscall_keccak_permute;
 
 pub fn main() {
-    for _ in 0..25 {
+    for _ in 0..5000 {
         let mut state = [1u64; 25];
         syscall_keccak_permute(state.as_mut_ptr());
         println!("{:?}", state);
