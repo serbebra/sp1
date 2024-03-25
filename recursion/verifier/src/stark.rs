489809ccbe2107bdb8cea8e32f3ad82f2ae6df95
Tamir Hemo <tamir@succinct.xyz>
2024-03-25 11:48:21 -0700

    try

--- recursion/verifier/src/stark.rs
+++ recursion/verifier/src/stark.rs
@@ -2,15 +2,18 @@ use sp1_core::stark::StarkGenericConfig;
 use sp1_recursion_compiler::ir::Builder;
 
 pub struct StarkVerifier<'a, C, SC: StarkGenericConfig> {
-    builder: &'a mut Builder<SC::Val, SC::Challenge>,
+    builder: &'a mut Builder<C>,
 }
 
-impl<'a, SC: StarkGenericConfig, A> StarkVerifier<'a, SC, A> {
-    pub const fn new(builder: &'a mut Builder<SC::Val, SC::Challenge>) -> Self {
+impl<'a, C: Config, SC: StarkGenericConfig> StarkVerifier<'a, C, SC>
+where
+    SC: StarkGenericConfig<Val = C::F, Challenge = C::EF>,
+{
+    pub const fn new(builder: &'a mut Builder<C>) -> Self {
         Self { builder }
     }
 
-    pub fn verify_shard(
+    pub fn verify_shard<A>(
         &mut self,
         chips: &[&MachineChip<SC, A>],
         challenger: &mut SC::Challenger,
