436f74ea65408ad12dd606e34bc4d5484e635093
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-04-22 11:13:24 -0700

    wip

---
+++ examples/book/program/src/main.rs
@@ -0,0 +1,37 @@
+//! A simple program to be proven inside the zkVM.
+#![no_main]
+sp1_zkvm::entrypoint!(main);
+
+use reth_primitives::{Block, Header};
+use serde::{Deserialize, Serialize};
+
+pub fn main() {
+    // NOTE: values of n larger than 186 will overflow the u128 type,
+    // resulting in output that doesn't match fibonacci sequence.
+    // However, the resulting proof will still be valid!
+    println!("cycle-tracker-start: r0 serde");
+    let header = sp1_zkvm::io::read::<Block>();
+    println!("cycle-tracker-end: r0 serde");
+
+    println!("cycle-tracker-start: bincode serde");
+    let bincode_serialized_header = sp1_zkvm::io::read_vec();
+    let bincode_header: Block = bincode::deserialize(&bincode_serialized_header).unwrap();
+    println!("cycle-tracker-end: bincode serde");
+
+    println!("cycle-tracker-start: serde_cbor serde");
+    let serde_serialized_header = sp1_zkvm::io::read_vec();
+    let serde_header: Block = serde_cbor::from_slice(&serde_serialized_header).unwrap();
+    println!("cycle-tracker-end: serde_cbor serde");
+
+    // let mut a: u128 = 0;
+    // let mut b: u128 = 1;
+    // let mut sum: u128;
+    // for _ in 1..n {
+    //     sum = a + b;
+    //     a = b;
+    //     b = sum;
+    // }
+
+    // sp1_zkvm::io::commit(&a);
+    // sp1_zkvm::io::commit(&b);
+}
